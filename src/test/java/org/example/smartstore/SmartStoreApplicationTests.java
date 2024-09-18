c24357846237cc6836eea031fdc032d48783eb80
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-09-18 16:11:43 -0300

    feat: add spring boot project

---
+++ src/test/java/org/example/smartstore/SmartStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
